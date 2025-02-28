6ae3529a23fe6ed6e62330bc354a43cbea3fef40
clifforddonk <clifforddonk@gmail.com>
2025-02-28 09:08:30 +0000

    new changes

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
-  return <div>This is the homepage</div>;
+  return (
+    <div>
+      This is the homepage
+      <Link href="/user/dashboard">
+        <button>click here for dashboard</button>
+      </Link>
+    </div>
+  );
 };
 
 export default page;
