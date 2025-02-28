3b60df9c8fb86d886dc4b6d883b0577ca1d3dc5f
clifforddonk <clifforddonk@gmail.com>
2025-02-28 09:56:36 +0000

    project setup

--- app/dashboard/user/page.jsx
+++ app/dashboard/user/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
+    <div className="font-nunito">
       Welcome to your dashboard
       <Link href="/home">back</Link>
     </div>
