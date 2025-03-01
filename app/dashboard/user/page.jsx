49231f6ebdc5dd02f5f4cf6e10b2a3892684a3e4
clifforddonk <clifforddonk@gmail.com>
2025-03-01 18:55:02 +0000

    fix nextauth

--- app/dashboard/user/page.jsx
+++ app/dashboard/user/page.jsx
@@ -7,10 +7,17 @@ const page = () => {
   const { data: session } = useSession();
 
   return (
-    <div className="font-nunito ">
-      Welcome {session?.user?.name}
-      <Link href="/dashboard/about">Click here About Page</Link>
-      <button onClick={() => signOut({ callbackUrl: "/auth/login" })}>
+    <div className="font-nunito flex items-center justify-center flex-col ">
+      <h1 className="font-bold text-xl">Welcome, {session?.user?.name}😎</h1>
+
+      <div className="hover:underline">
+        <Link href="/dashboard/about ">Click here for About Page</Link>
+      </div>
+
+      <button
+        className="bg-blue-500 rounded-lg max-w-md hover:bg-blue-400 text-white font-semibold px-4 py-2 cursor-pointer transition"
+        onClick={() => signOut({ callbackUrl: "/auth/login" })}
+      >
         SignOut
       </button>
     </div>
