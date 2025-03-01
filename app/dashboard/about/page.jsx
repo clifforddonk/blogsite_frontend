bf2b4a38364118eb4fffa9cf2216be28bbce7a3e
clifforddonk <clifforddonk@gmail.com>
2025-03-01 13:46:38 +0000

    Add Redirections

--- app/dashboard/about/page.jsx
+++ app/dashboard/about/page.jsx
@@ -1,9 +1,14 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { useSession } from "next-auth/react";
 
 const page = () => {
+  const { data: session } = useSession();
+
   return (
     <div>
+      Welcome {session?.user?.name}
       <Link href="/dashboard/user">return to user dashboard</Link>
     </div>
   );
