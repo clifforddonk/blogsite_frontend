3db5ef9e205f174ff5b76f90a69f0f5307fdcee5
clifforddonk <clifforddonk@gmail.com>
2025-03-01 13:46:44 +0000

    Add Redirections

--- app/dashboard/user/page.jsx
+++ app/dashboard/user/page.jsx
@@ -1,11 +1,18 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useCallback } from "react";
+import { signOut, useSession } from "next-auth/react";
 
 const page = () => {
+  const { data: session } = useSession();
+
   return (
     <div className="font-nunito ">
-      Welcome to your dashboard
-      <Link href="/dashboard/about">About Page</Link>
+      Welcome {session?.user?.name}
+      <Link href="/dashboard/about">Click here About Page</Link>
+      <button onClick={() => signOut({ callbackUrl: "/auth/login" })}>
+        SignOut
+      </button>
     </div>
   );
 };
