01922a6e8a415578d9676f5343d864aacbcb20db
clifforddonk <clifforddonk@gmail.com>
2025-03-02 21:40:25 +0000

    create UserModel

---
+++ app/api/auth/register/route.js
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+
+const prisma = new PrismaClient();
+export async function POST(req) {
+  try {
+    const { name, email, password } = await req.json();
+
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) {
+      return Response.json({ error: "User already exists" }, { status: 400 });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: { name, email, password: hashedPassword },
+    });
+
+    return Response.json({
+      message: "User created successfully",
+      user: newUser,
+    });
+  } catch (error) {
+    return Response.json({ error: "Error creating user" }, { status: 500 });
+  }
+}
