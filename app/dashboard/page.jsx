4999ae9c3952c0b64cad0914fbc2e05729ddac56
clifforddonk <clifforddonk@gmail.com>
2025-03-05 23:15:19 +0000

    Improve SideBar Component

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,22 +1,9 @@
-"use client";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import DashboardPosts from "../components/dashboard/DashboardPosts";
 
-export default function Dashboard() {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (status === "unauthenticated") {
-      router.push("/auth/login");
-    } else if (session) {
-      router.push("/dashboard/user");
-    }
-  }, [status, session, router]);
-
-  if (status === "loading") {
-    return <p>Loading......</p>;
-  }
-  return null;
+export default function Home() {
+  return (
+    <div>
+      <DashboardPosts /> {/* Client Component */}
+    </div>
+  );
 }
